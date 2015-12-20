37805e918fc396a54fa4fa487765b45522e5518c
Valentin Montupet <valentin.mntp@gmail.com>
2015-12-20 14:13:22 +0100

    Ajout règles

---
+++ br.lisp
@@ -0,0 +1,198 @@
+
+(setq R1 '(num1
+	((SEMESTRE 2) (NF16 non_validee))
+	(NB_UV 7)
+	)
+)
+
+(setq R2 '(num2
+	((SEMESTRE 1) (PROVENANCE IUT))
+	(MB11 conseillee)
+	)
+)
+
+(setq R3 '(num3
+	((SEMESTRE 1) (PROVENANCE TC))
+	(NF16 conseillee)
+	)
+)
+
+(setq R4 '(num4
+	((SEMESTRE 2) (SR01 validee) (SAISON P))
+	(SR02 conseillee)
+	)
+)
+
+(setq R5 '(num5
+	((SEMESTRE 2) (NF16 validee))
+	(NF17 conseillee)
+	)
+)
+
+(setq R6 '(num6
+	((SEMESTRE 2) (IA01 validee) (SAISON P))
+	(IA02 conseillee)
+	)
+)
+
+(setq R7 '(num7
+	((SEMESTRE 4) (< CreditsCS 30))
+	(CS 3)
+	)
+)
+
+(setq R8 '(num8
+	((SEMESTRE 4) (< CreditsTM 30))
+	(TM 3)
+	)
+)
+
+(setq R9 '(num9
+	((LA12 validee) (LA13 non_validee))
+	(LA13 conseillee)
+	)
+)
+
+(setq R10 '(num10
+	((NB_UV 7) (SEMESTRE 2) (< CreditsTM 12))
+	(TM 3)
+	)
+)
+
+(setq R11 '(num11
+	((NB_UV 7) (SEMESTRE 2) (< CreditsCS 12))
+	(CS 3)
+	)
+)
+
+(setq R12 '(num12
+	((CS 3) (NF16 non_validee))
+	(NF16 conseillee)
+	)
+)
+
+(setq R13 '(num13
+	((FILIERE SRI) (SR01 non_validee) (SAISON A))
+	(SR01 conseillee)
+	)
+)
+
+(setq R14 '(num14
+	((FILIERE SRI) (SR02 non_validee) (SAISON P))
+	(SR02 conseillee)
+	)
+)
+
+(setq R15 '(num15
+	((FILIERE SRI) (SR04 non_validee) (SAISON A))
+	(SR04 conseillee)
+	)
+)
+
+(setq R16 '(num16
+	((FILIERE SRI) (SR05 non_validee) (SAISON P))
+	(SR05 conseillee)
+	)
+)
+
+(setq R17 '(num17
+	((FILIERE SRI) (SR06 non_validee) (SAISON A))
+	(SR06 conseillee)
+	)
+)
+
+(setq R18 '(num18
+	((FILIERE SRI) (SR03 non_validee) (SAISON P))
+	(SR03 conseillee)
+	)
+)
+
+(setq R19 '(num19
+	((FILIERE ADEL) (MP03 non_validee) (SAISON A))
+	(MP03 conseillee)
+	)
+)
+
+(setq R20 '(num20
+	((FILIERE ADEL) (RO06 non_validee) (SAISON A))
+	(RO06 conseillee)
+	)
+)
+
+(setq R21 '(num21
+	((FILIERE ADEL) (MP02 non_validee) (SAISON A))
+	(MP02 conseillee)
+	)
+)
+
+(setq R22 '(num22
+	((FILIERE FDD) (SY09 non_validee) (SAISON P))
+	(SY09 conseillee)
+	)
+)
+
+(setq R23 '(num23
+	((FILIERE FDD) (NF26 non_validee) (SAISON P))
+	(NF26 conseillee)
+	)
+)
+
+(setq R24 '(num24
+	((FILIERE FDD) (SY19 non_validee) (SAISON A))
+	(SY19 conseillee)
+	)
+)
+
+(setq R25 '(num25
+	((FILIERE ICSI) (IA04 non_validee) (SAISON P))
+	(IA04 conseillee)
+	)
+)
+
+(setq R26 '(num26
+	((FILIERE ICSI) (LO17 non_validee) (SAISON P))
+	(LO17 conseillee)
+	)
+)
+
+(setq R27 '(num27
+	((FILIERE ICSI) (IA03 non_validee) (SAISON A))
+	(IA03 conseillee)
+	)
+)
+
+(setq R28 '(num28
+	((FILIERE ICSI) (NF28 non_validee) (SAISON P))
+	(NF28 conseillee)
+	)
+)
+
+(setq R29 '(num29
+	((FILIERE ICSI) (NF29 non_validee) (SAISON A))
+	(NF29 conseillee)
+	)
+)
+
+(setq R30 '(num30
+	((FILIERE STRIE) (SY15 non_validee) (SAISON P))
+	(SY15 conseillee)
+	)
+)
+
+(setq R31 '(num31
+	((FILIERE STRIE) (MI11 non_validee) (SAISON P))
+	(MI11 conseillee)
+	)
+)
+
+(setq R32 '(num32
+	((FILIERE STRIE) (MI12 non_validee) (SAISON P))
+	(MI12 conseillee)
+	)
+)
+
+(setq R33 '(num33
+	((FILIERE STRIE) (SY23 non_validee) (SAISON A))
+	(SY23 conseillee)
+	)
+)
